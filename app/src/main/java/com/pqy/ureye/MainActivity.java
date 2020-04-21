84b36d597bc640847f34009c734d0c08287b52e0
lee <603820467@qq.com>
2020-04-21 20:57:36 +0800

    init

---
+++ app/src/main/java/com/pqy/ureye/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pqy.ureye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
